3ff9ac8a34ef5cb28ef25e0c8bba9de08c4fbb52
Kraft55 <119878901+kraft55@users.noreply.github.com>
2024-03-26 13:10:12 +0100

    Extending basic functionality

--- fraktionslos.py
+++ fraktionslos.py
@@ -17,14 +17,26 @@ def get_nichtAbgegeben(row):
 
 
 def count_votes(df):
-	list = []
-
-
+	output = []
+	output.append(count_ja(df))
+	output.append(count_nein(df))
+	output.append(count_ungueltig(df))
+	output.append(count_nichtAbgegeben(df))
+	return output
 
 data = pd.read_csv('./output.csv')
+parteien_liste = data['Fraktion/Gruppe'].unique()
 
-sitzungsnummern = data['Sitzungsnr'].unique()
+output_df = pd.DataFrame()
+
+sitzungsnummern = data['Sitzungnr'].unique()
 
 for sitzungsnr in sitzungsnummern:
-	df_sitzungsnr = data[data['Sitzungsnr'] == sitzungsnr]
-	print(sitzungsnr, df_sitzungsnr.head())
+	df_sitzungsnr = data[data['Sitzungnr'] == sitzungsnr]
+	unique_stimmnr = df_sitzungsnr['Abstimmnr'].unique()
+	for stimmnr in unique_stimmnr:
+		df_sitzungsnr_stimmnr = df_sitzungsnr[df_sitzungsnr['Abstimmnr'] == stimmnr]
+		for partei in parteien_liste:
+			stimmen_partei = [sitzungsnr, stimmnr, partei]
+			df_sitzungsnr_stimmnr_partei = df_sitzungsnr_stimmnr[df_sitzungsnr_stimmnr['Fraktion/Gruppe'] == partei]
+			stimmen_partei.extend(count_votes(df_sitzungsnr_stimmnr_partei))
