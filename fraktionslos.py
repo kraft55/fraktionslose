b393c2323058f0f74e2dc906877207074a537c2b
Kraft55 <119878901+kraft55@users.noreply.github.com>
2024-03-26 13:16:01 +0100

    Add vote counting methods

--- fraktionslos.py
+++ fraktionslos.py
@@ -1,19 +1,51 @@
 import pandas as pd
 
 def get_ja(row):
-	return row['ja']
+	return row['ja'] == True
 
 def get_nein(row):
-	return row['nein']
+	return row['nein'] == True
 
 def get_enthaltung(row):
-	return row['enthaltung']
+	return row['Enthaltung'] == True
 
 def get_ungueltig(row):
-	return row['ungültig']
+	return row['ungültig'] == True
 
 def get_nichtAbgegeben(row):
-	return row['nichtabgegeben']
+	return row['nichtabgegeben'] == True
+
+
+def count_ja(df):
+	count = 0
+	for i, row in df.iterrows():
+		if get_ja(row):
+			count += 1
+	return count
+
+def count_nein(df):
+	count = 0
+	for i, row in df.iterrows():
+		if get_nein(row):
+			count += 1
+	return count
+
+def count_ungueltig(df):
+	count = 0
+	for i, row in df.iterrows():
+		if get_enthaltung(row):
+			count += 1
+	return count
+
+def count_nichtAbgegeben(df):
+	count = 0
+	for i, row in df.iterrows():
+		if get_nichtAbgegeben(row):
+			count += 1
+	return count
+
+
+
 
 
 def count_votes(df):
@@ -40,3 +72,4 @@ for sitzungsnr in sitzungsnummern:
 			stimmen_partei = [sitzungsnr, stimmnr, partei]
 			df_sitzungsnr_stimmnr_partei = df_sitzungsnr_stimmnr[df_sitzungsnr_stimmnr['Fraktion/Gruppe'] == partei]
 			stimmen_partei.extend(count_votes(df_sitzungsnr_stimmnr_partei))
+			print(stimmen_partei)
