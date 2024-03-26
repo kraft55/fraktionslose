bd583fe78dfdb7d6f7e39b689040f51dc24f21ae
Kraft55 <119878901+kraft55@users.noreply.github.com>
2024-03-26 12:47:50 +0100

    Add basic functionality

--- fraktionslos.py
+++ fraktionslos.py
@@ -16,5 +16,15 @@ def get_nichtAbgegeben(row):
 	return row['nichtabgegeben']
 
 
-def count_votes(series):
-	return []
+def count_votes(df):
+	list = []
+
+
+
+data = pd.read_csv('./output.csv')
+
+sitzungsnummern = data['Sitzungsnr'].unique()
+
+for sitzungsnr in sitzungsnummern:
+	df_sitzungsnr = data[data['Sitzungsnr'] == sitzungsnr]
+	print(sitzungsnr, df_sitzungsnr.head())
